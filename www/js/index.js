27d6874b2d01819ca67282292ddf0f918b0b9041
PabloRuiz <pabloruizsoria@gmail.com>
2016-09-15 00:26:05 +0200

    Empiezo a trastear con la camara

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 var app = {
-    
+
   initialize: function() {
     console.log('Inicializando app');
     document.addEventListener('deviceready', this.onDeviceReady, false);
@@ -7,6 +7,22 @@ var app = {
   },
   onDeviceReady: function() {
     console.log('Dispositivo preparado');
+    $('#btnFoto').click(function(event) {
+      console.log('Click en el bot´on btnFoto');
+      console.log(navigator.camera);
+      navigator.camera.getPicture(cameraSuccess, cameraError, {
+        // Some common settings are 20, 50, and 100 
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery 
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks 
+    });
+    });
+
   }
 };
 
